fa045ea8f7387b8565943165340925730d56f321
Ernani Raffo <eraffo@ucsc.edu>
2022-04-28 15:50:29 -0700

    better platform and window transitions

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,6 +8,7 @@ class Play extends Phaser.Scene {
         this.load.image("cloud", "./assets/cloud.png");
         this.load.image("skyscraper", "./assets/skyscraper.png");
         this.load.spritesheet('hero', './assets/player1.png', {frameWidth: 32, frameHeight: 32, startFrame: 0, endFrame: 1});
+        this.load.image("window", "./assets/window.png");
     }
     create() {
 
@@ -16,16 +17,20 @@ class Play extends Phaser.Scene {
         
         // platform physics group
         this.platformGroup = this.physics.add.group();
+        
+        this.windowGroup = this.physics.add.group();
 
         // create starting platform
         let platform = this.platformGroup.create(game.config.width / 2, game.config.height - 40, "platform");
+        this.windowGroup.create(game.config.width / 2, platform.y - 55, "window");
         platform.setImmovable(true);
 
         // create platforms
         for(let i = 0; i < 3; i ++) {
             let platform = this.platformGroup.create(640, 480, "platform");
+            let window = this.windowGroup.create(0, 0, "window");
             platform.setImmovable(true);
-            this.positionPlatform(platform)
+            this.positionPlatform(platform, window);
         }
 
         // add the hero
@@ -38,6 +43,7 @@ class Play extends Phaser.Scene {
 
         // set hero gravity
         this.hero.body.gravity.y = gameOptions.gameGravity;
+        this.hero.depth = 1; // puts player to front
 
         // set the appropriate keys
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
@@ -51,9 +57,6 @@ class Play extends Phaser.Scene {
 
         // we are waiting for player first move
         this.firstMove = true;
-
-        
-
     }
 
     // method to return a random value between index 0 and 1 of a giver array
@@ -82,6 +85,7 @@ class Play extends Phaser.Scene {
         if(this.firstMove) {
             this.firstMove = false;
             this.platformGroup.setVelocityY(-gameOptions.platformSpeed);
+            this.windowGroup.setVelocityY(-gameOptions.platformSpeed);
         }
     }
 
@@ -104,7 +108,7 @@ class Play extends Phaser.Scene {
     }
 
     // method to position a platform
-    positionPlatform(platform) {
+    positionPlatform(platform, window) {
 
         // vertical position
         platform.y = this.getHighestPlatform() - this.randomValue(gameOptions.platformVerticalDistanceRange);
@@ -115,10 +119,13 @@ class Play extends Phaser.Scene {
         // horizontal position
         platform.x = game.config.width / 2 + this.randomValue(gameOptions.platformHorizontalDistanceRange) * platformToggle;
 
-
+        window.y = platform.y - 55;
+        window.x = platform.x;
 
         // platform width
-        platform.displayWidth = this.randomValue(gameOptions.platformLengthRange);
+        let platform_length = this.randomValue(gameOptions.platformLengthRange);
+        platform.displayWidth = platform_length;
+        window.displayWidth = platform_length;
     }
 
 
@@ -138,15 +145,24 @@ class Play extends Phaser.Scene {
         // handle collision between player and platforms
         this.physics.world.collide(this.platformGroup, this.hero);
 
+        this.windowGroup.getChildren().forEach(function(window) {
+            if (window.getBounds().top > game.config.height && window.active) {
+                window.active = false; // set window child as false
+            }
+        });
+
         // loop through all platforms
         this.platformGroup.getChildren().forEach(function(platform) {
-
             // if a platform leaves the stage to the upper side...
-            if(platform.getBounds().bottom > game.config.height) {
-
+            if(platform.getBounds().top > game.config.height) {
+                let window = this.windowGroup.getFirstDead(true, 100, 100, "window", 0, true);
+                window.active = true;
+                //let window = this.physics.add.sprite(0, 0, "window");
                 // ... recycle the platform
-                this.positionPlatform(platform);
-                platform.y = 0; // platforms spawn at the top
+                this.positionPlatform(platform, window);
+                platform.y = -1; // platforms spawn at the top
+                window.y = platform.y - 56;
+                window.setVelocityY(-gameOptions.platformSpeed);
             }
         }, this);
 
